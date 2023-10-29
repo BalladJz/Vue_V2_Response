779819e525c707c16f0666c53dbb2fe1d10dc6e3
Ballad <751464436@qq.com>
2023-10-29 21:48:38 +0800

    完成数据的响应式处理

--- src/index.js
+++ src/index.js
@@ -6,7 +6,25 @@ function Vue(options) {
   // options.data可能是函数，也可能是一个对象
   this._data =
     typeof options.data === 'function' ? options.data() : options.data
-  
+
+  // 实现数据的响应式（双向绑定）
+  // Object.keys(this._data).forEach((key) => {
+  //   let val = undefined
+  //   let cb = undefined
+  //   Object.defineProperty(this._data, key, {
+  //     get() {
+  //       console.log('get => 获取数据', val)
+  //       return val
+  //     },
+  //     set(value) {
+  //       console.log('set => 更新数据', value)
+  //       val = value
+  //       cb ? cb(val) : null
+  //     },
+  //   })
+  // })
+  observe(this._data)
+
   // 定义 代理的方法
   // 将Vue实例上的操作，代理到Vue._data上
   // Object.keys(this._data).forEach((key) => {
@@ -23,6 +41,7 @@ function Vue(options) {
   _proxy.call(this, this._data)
 }
 
+// 定义代理的方法
 function _proxy(data) {
   Object.keys(data).forEach((key) => {
     Object.defineProperty(this, key, {
@@ -33,6 +52,27 @@ function _proxy(data) {
         this._data[key] = value
       },
     })
+  })
+}
+
+// 定义响应式数据
+function observe(value, cb) {
+  Object.keys(value).forEach((key) => {
+    defineReactive(value, key, value[key], cb)
+  })
+}
+
+function defineReactive(obj, key, val, cb) {
+  Object.defineProperty(obj, key, {
+    get: () => {
+      console.log('get => 获取数据', val)
+      return val
+    },
+    set: (value) => {
+      console.log('set => 更新数据', value)
+      val = value
+      cb ? cb(value) : null
+    },
   })
 }
 
